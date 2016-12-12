cb3f021d7b4a1b64ad57d08a611dab34d2f28ee0
Eduard Fidiles <eduard@fathomlondon.com>
2016-12-12 01:30:27 +0000

    Update example

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 import { OpaqueToken } from '@angular/core';
 export declare const ENHANCED_COMPONENT_INITIALIZER: OpaqueToken;
-export declare function EnhacedComponent<T>(config: T): (targetConstructor: any) => any;
+export declare function EnhancedComponent<T>(config: T): (targetConstructor: any) => any;
